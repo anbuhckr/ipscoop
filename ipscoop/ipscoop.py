35cc3eb4545b74ce4276aade38817e2a93c4fe14
anbuhckr <36891836+anbuhckr@users.noreply.github.com>
2021-06-17 12:30:36 +0700

    Update ipscoop.py

--- ipscoop/ipscoop.py
+++ ipscoop/ipscoop.py
@@ -1,23 +1,15 @@
 #! /usr/bin/env python3
 
-import maxminddb, os, sys
-from argparse import ArgumentParser
-
-parser = ArgumentParser()    
-parser.add_argument('-data', help="show ip data", default=None, type=str)
-parser.add_argument('-cidr', help="show ip cidr", default=None, type=str)
-parser.add_argument('-range', help="show ip range", default=None, type=str)
-parser.add_argument('-country', help="show ip country", default=None, type=str)
-parser.add_argument('-tzid', help="show ip timezone", default=None, type=str)
-parser.add_argument('-geo', help="show ip geo", default=None, type=str)
-args = parser.parse_args()
+import maxminddb, os, urllib3
 
 PACKAGE_DIR = os.path.dirname(os.path.realpath(__file__))
 DB_FILE = os.path.join(PACKAGE_DIR, 'rbs_db.mmdb')
 
 class IpScoop():
 
-    def __init__(self, ip:str) -> None:
+    def __init__(self, ip:str) -> None:        
+        if not os.path.exists(DB_FILE):
+            self.download_data()
         self.ip = ip
         self.reader = maxminddb.open_database(DB_FILE)
         self.data = self.reader.get(self.ip)
@@ -27,36 +19,10 @@ class IpScoop():
         self.tzid = self.data['tzid']
         self.geo = {'lat': self.data['lat'], 'lon': self.data['lon']}
         
-def download_data():
-    import urllib3
-    urllib3.disable_warnings()
-    url = 'https://github.com/anbuhckr/ipscoop/releases/download/v0.1.0/rbs_db.mmdb'
-    with urllib3.PoolManager() as http:
-        r = http.request('GET', url)
-        with open(DB_FILE, 'wb') as fout:
-            fout.write(r.data)
-        
-if __name__ == '__main__':
-    if not os.path.exists(DB_FILE):
-        download_data()
-    if args.data:
-        ip_scoop = IpScoop(args.data)
-        print(ip_scoop.data)
-    elif args.cidr:
-        ip_scoop = IpScoop(args.cidr)
-        print(ip_scoop.cidr)
-    elif args.range:
-        ip_scoop = IpScoop(args.range)
-        print(ip_scoop.range) 
-    elif args.country:
-        ip_scoop = IpScoop(args.country)
-        print(ip_scoop.country)    
-    elif args.tzid:
-        ip_scoop = IpScoop(args.tzid)
-        print(ip_scoop.tzid)   
-    elif args.geo:
-        ip_scoop = IpScoop(args.geo)
-        print(ip_scoop.geo)
-    else:
-        print('usage: ipscoop.py [-h]')   
-    sys.exit()
+    def download_data(self):   
+        urllib3.disable_warnings()
+        url = 'https://github.com/anbuhckr/ipscoop/releases/download/v0.1.0/rbs_db.mmdb'
+        with urllib3.PoolManager() as http:
+            r = http.request('GET', url)
+            with open(DB_FILE, 'wb') as fout:
+                fout.write(r.data)
