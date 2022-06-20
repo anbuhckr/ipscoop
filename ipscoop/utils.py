d37505aa89b99fe12e8a9feae346ee9d56a90a0a
anbuhckr <36891836+anbuhckr@users.noreply.github.com>
2022-06-20 11:44:42 +0700

    Update utils.py

--- ipscoop/utils.py
+++ ipscoop/utils.py
@@ -4,10 +4,11 @@ import os, gzip, base64, requests
 from tqdm import tqdm
 
 PACKAGE_DIR = os.path.dirname(os.path.realpath(__file__))
+GZ_FILE = os.path.join(PACKAGE_DIR, 'ipscoop.mmdb.gz')
 DB_FILE = os.path.join(PACKAGE_DIR, 'ipscoop.mmdb')
 
 def gunzip():
-    with gzip.open('ipscoop.mmdb.gz', 'rb') as inp, open('ipscoop.mmdb', 'wb') as out:
+    with gzip.open(GZ_FILE, 'rb') as inp, open(DB_FILE, 'wb') as out:
         while True:
             block = inp.read(65536)
             if not block:
@@ -21,13 +22,13 @@ def download():
     total_size_in_bytes= int(response.headers.get('content-length', 0))
     block_size = 1024
     progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True, desc='Download ipscoop.mmdb.gz')
-    with open(DB_FILE, 'wb') as f:
+    with open(GZ_FILE, 'wb') as f:
         for data in response.iter_content(block_size):
             progress_bar.update(len(data))
             f.write(data)
     progress_bar.close()
     if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
-        os.remove(DB_FILE)
+        os.remove(GZ_FILE)
         print('Failed to download, check your connection!')
     else:
         print('Extract ipscoop.mmdb.gz...')
